65003300ed790db2706ccdf67c54a49ab44784c6
blackholebug <blackholebug@gmail.com>
2020-01-11 23:37:10 +0100

    experiment 2

--- comp_rgrs/run.sh
+++ comp_rgrs/run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-nohup python3 0_boston.py > 0_boston.txt &
 nohup python3 0_diabetes.py > 0_diabetes.txt &
 nohup python3 0_linnerud.py > 0_linnerud.txt &
 nohup python3 0_california.py > 0_california.txt &
