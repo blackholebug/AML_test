ef781f4fd12d684f9caa29e80c666cc2719b9383
blackholebug <blackholebug@gmail.com>
2020-01-12 10:54:37 +0100

    comparison of skl

--- comp_rgrs/run.sh
+++ comp_rgrs/run.sh
@@ -1,4 +1,30 @@
 #!/bin/bash
-nohup python3 0_diabetes.py > 0_diabetes.txt &
-nohup python3 0_linnerud.py > 0_linnerud.txt &
-nohup python3 0_california.py > 0_california.txt &
+source activate amlenv
+cd Mytest/comp_rgrs
+
+nohup python3 1_eskl.py > 1_eskl.txt 2>&1 &
+wait
+nohup python3 2_eskl.py > 2_eskl.txt 2>&1 &
+wait
+nohup python3 3_eskl.py > 3_eskl.txt 2>&1 &
+wait
+nohup python3 4_eskl.py > 4_eskl.txt 2>&1 &
+wait
+nohup python3 5_eskl.py > 5_eskl.txt 2>&1 &
+wait
+nohup python3 10_eskl.py > 10_eskl.txt 2>&1 &
+wait
+nohup python3 15_eskl.py > 15_eskl.txt 2>&1 &
+wait
+nohup python3 20_eskl.py > 20_eskl.txt 2>&1 &
+wait
+nohup python3 25_eskl.py > 25_eskl.txt 2>&1 &
+wait
+nohup python3 30_eskl.py > 30_eskl.txt 2>&1 &
+wait
+nohup python3 40_eskl.py > 40_eskl.txt 2>&1 &
+wait
+nohup python3 50_eskl.py > 50_eskl.txt 2>&1 &
+wait
+nohup python3 60_eskl.py > 60_eskl.txt 2>&1 &
+wait
---
+++ comp_skl/5_eskl.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 10 14:21:21 2020
+
+@author: s2432234
+"""
+import time
+
+from autosklearn.classification import AutoSklearnClassifier
+from autosklearn.regression import AutoSklearnRegressor
+from sklearn.model_selection import train_test_split
+
+import sklearn.datasets as ds
+
+from sklearn.metrics import classification_report
+from sklearn.metrics import balanced_accuracy_score
+from sklearn.metrics import r2_score
+from sklearn.metrics import mean_squared_error
+
+rgr_dataset = [ds.load_boston, ds.load_diabetes, ds.load_linnerud, ds.fetch_california_housing]
+
+print('[INFO] Loading dataset.')
+
+X, y = rgr_dataset[3](return_X_y=True)
+#feature_types = (['numerical'] * 3) + ['categorical'] + (['numerical'] * 9)
+ 
+print('[INFO] Splitting.')
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, train_size=0.8)
+ 
+print('[INFO] Train shape: {}'.format(X_train.shape))
+print('[INFO] Test shape: {}'.format(X_test.shape))
+print('[INFO] Finding best model...')
+
+#for auto vanilla, add this : #ensemble_size=1, initial_configurations_via_metalearning=0
+#for specific clf or rgr or prep :     include_estimators=["random_forest", ], exclude_estimators=None,
+#                                      include_preprocessors=["no_preprocessing", ], exclude_preprocessors=None)
+
+#-----CLASSIFIER-----
+#automl = AutoSklearnClassifier(per_run_time_limit=300, ml_memory_limit=1024 * 6, 
+#                               time_left_for_this_task=300, resampling_strategy='cv',
+#                               ensemble_size=1, initial_configurations_via_metalearning=0, 
+#                               resampling_strategy_arguments={'folds': 5})
+
+#-----REGRESSION-----
+automl = AutoSklearnRegressor(per_run_time_limit=360, ml_memory_limit=1024 * 8, 
+                              time_left_for_this_task=300, resampling_strategy='cv', 
+                              ensemble_size=1, 
+#                              initial_configurations_via_metalearning=0, 
+                              resampling_strategy_arguments={'folds': 5})
+start = time.time()
+ 
+#X_train = X_train.astype('float') # when?
+automl.fit(X_train, y_train, dataset_name='boston_housing')   #change dataset name accordingly
+automl.refit(X_train.copy(), y_train.copy())
+print('[INFO] Elapsed time finding best model: {} seconds.'.format(time.time() - start)) 
+
+predictions = automl.predict(X_test)
+#print('--- CLASSIFICATION REPORT: ---')        #not for regression
+#print(classification_report(y_test, predictions, digits=5))
+print('\n\n--- MODELS: ---')
+print(automl.show_models())
+print('\n\n--- STATISTICS: ---')
+print(automl.sprint_statistics()) 
+
+#-----CLASSIFIER-----
+#print('\n\n--- SCORE: ---')
+#print("Balanced error score", 1 - balanced_accuracy_score(y_test, predictions))
+
+#-----REGRESSION-----
+print('\n\n--- SCORE: ---')
+print("R2 score", r2_score(y_test, predictions))
